8667c2d7fc4769842317f392e823026cacbab687
silviasalete <silviasalete@users.noreply.github.com>
2021-07-19 17:55:29 -0300

    :fire: Removed data.sql

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `okr`.`usr` (`email`, `password`, `username`) VALUES( "teste@email.com", "123", "Test");
-INSERT INTO `okr`.`objctv` (`created_in`,`description`,`title`,`updated_in`,`created_by_id`,`updated_by_id`)
-VALUES (NOW(), "Description Default", "Title Default", NOW(), 1, 1);
-INSERT INTO `okr`.`objctv` (`created_in`,`description`,`title`,`updated_in`,`created_by_id`,`updated_by_id`)
-VALUES (NOW(), "Description Default", "Title Default", NOW(), 1, 1);
